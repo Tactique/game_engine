7597b5d6beaa76d06a3b7a62a7a730611d17c3c0
Quinten <quintenpalmer@gmail.com>
2013-11-07 08:23:51 -0500

    Include test runner

---
+++ check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#set -e
+
+function check_ok {
+    if [ $? -ne 0 ]; then
+        echo $1
+        echo "FAILED"
+        exit 1
+    fi
+}
+
+find . -name "*.py" | xargs pep8
+check_ok "pep8"
+
+for package in 'lib' 'engine'; do
+    for module in ${package}/*; do
+        module=$(echo ${module} | sed "s/${package}\/\(.*\).py/\1/")
+        echo ${module}
+        if [ ${module} != '__init__' ]; then
+            coverage run --source=${package}.${module} tests/${package}_test/${module}_test.py
+            check_ok "Unit Test"
+            coverage report --fail-under=100 -m
+            check_ok "Code Coverage"
+            coverage erase
+        fi
+    done
+done
+echo "All Done:"
+echo "OK"
