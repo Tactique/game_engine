e5ee061bbefdae0823f1c4f7261c41ae4b3812ba
Quinten <quintenpalmer@gmail.com>
2013-11-12 18:46:14 -0500

    Improve moving checking

--- engine/move.py
+++ engine/move.py
@@ -3,9 +3,28 @@ from lib import contract
 from . import movement
 
 
-@contract.accepts(int, movement.Movement, list)
+@contract.accepts(int, int)
+@contract.returns(int)
+def get_distance(first, second):
+    return abs(first - second)
+
+
+@contract.accepts(list)
 @contract.returns(bool)
-def valid_move(moves, movement_type, tiles):
+def assert_all_tiles_touch(move_list):
+    old_x, old_y = move_list[0]
+    for x, y in move_list[1:]:
+        if (get_distance(x, old_x) + get_distance(y, old_y)) != 1:
+            return False
+        old_x, old_y = x, y
+    return True
+
+
+@contract.accepts(int, movement.Movement, list, list)
+@contract.returns(bool)
+def valid_move(moves, movement_type, tiles, move_list):
+    if not assert_all_tiles_touch(move_list):
+        return False
     for tile in tiles:
         moves -= movement_type[tile]
     return moves > 0
--- tests/engine_test/move_test.py
+++ tests/engine_test/move_test.py
@@ -7,13 +7,38 @@ from engine import move, movement, tile
 class MoveTest(unittest.TestCase):
     def testMoveSixTreadOverTwoPlain(self):
         valid = move.valid_move(
-            6, movement.Treads(), [tile.PLAIN, tile.PLAIN])
+            6, movement.Treads(), [tile.PLAIN, tile.PLAIN], [(1, 1), (1, 2)])
         self.assertTrue(valid)
 
     def testMoveTwoTreadOverTenPlain(self):
         invalid = move.valid_move(
-            2, movement.Treads(), [tile.PLAIN for i in range(10)])
+            2,
+            movement.Treads(),
+            [tile.PLAIN for i in range(10)],
+            [(1, x) for x in range(10)])
         self.assertFalse(invalid)
+
+    def testValidMoveInvalidTiles(self):
+        invalid = move.valid_move(
+            2,
+            movement.Treads(),
+            [tile.PLAIN for i in range(10)],
+            [(1, x * 2) for x in range(10)])
+        self.assertFalse(invalid)
+
+    def testGetDistance(self):
+        self.assertEqual(move.get_distance(3, 2), 1)
+        self.assertEqual(move.get_distance(1, 2), 1)
+        self.assertEqual(move.get_distance(1, 1), 0)
+        self.assertEqual(move.get_distance(1, 4), 3)
+        self.assertEqual(move.get_distance(7, 4), 3)
+
+    def testAllTilesTouch(self):
+        self.assertEqual(move.assert_all_tiles_touch([(1, 1), (2, 2)]), False)
+        self.assertEqual(
+            move.assert_all_tiles_touch([(1, 1), (1, 2), (2, 2)]),
+            True)
+
 
 if __name__ == '__main__':
     unittest.main()
