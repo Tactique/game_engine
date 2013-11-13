cb95c6d427c903c33fe9269e0ccb395bda3ea8c4
Quinten <quintenpalmer@gmail.com>
2013-11-12 22:11:16 -0500

    Abstract Attack class structure

--- engine/attack.py
+++ engine/attack.py
@@ -3,8 +3,10 @@ from lib import contract
 BULLET = 0
 CANNON = 1
 
+from . import base
 
-class Attack(object):
+
+class Attack(base.BaseClass):
     @contract.self_accepts(int, int)
     def __init__(self, power, attackType):
         self.power = power
@@ -16,10 +18,6 @@ class Attack(object):
             type(self) == type(attack) and
             self.power == attack.power and
             self.attackType == attack.attackType)
-
-    @contract.returns(bool)
-    def __ne__(self, attack):
-        return not self.__eq__(attack)
 
 
 class RegularCannon(Attack):
