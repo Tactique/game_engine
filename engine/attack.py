483099f729e0a2796b4bde0cd97621e5ab24765f
Quinten <quintenpalmer@gmail.com>
2013-11-14 22:40:58 -0500

    Attacks are now parsed from json

--- engine/attack.py
+++ engine/attack.py
@@ -1,28 +1,37 @@
 from lib import contract
 
-BULLET = 0
-CANNON = 1
-
-from . import base
+from . import base, file_loader
 
 
 class Attack(base.BaseClass):
-    @contract.self_accepts(int, int)
-    def __init__(self, power, attackType):
+    @contract.self_accepts(str, int, int)
+    def __init__(self, name, power, attackType):
+        self.name = name
         self.power = power
         self.attackType = attackType
 
 
-class RegularCannon(Attack):
-    def __init__(self):
-        Attack.__init__(self, 5, CANNON)
+@contract.returns(dict)
+def load_attack_types():
+    attack_types = {}
+    attack_list = file_loader.read_and_parse_json('attack_types')[0]
+    for enumeration, attack_type in enumerate(attack_list):
+        attack_types[str(attack_type)] = enumeration
+    return attack_types
 
 
-class MachineGun(Attack):
-    def __init__(self):
-        Attack.__init__(self, 5, BULLET)
+@contract.accepts(dict)
+def load_attacks(attack_types):
+    args = {}
+    names = []
+    for attack_ in file_loader.read_and_parse_json('attacks'):
+        name = str(attack_['name'])
+        power = attack_['power']
+        attack_type = attack_types[str(attack_['attack_type'])]
+        names.append(name)
+        args[name] = [name, power, attack_type]
 
-
-class DoubleMachineGun(Attack):
-    def __init__(self):
-        Attack.__init__(self, 10, BULLET)
+    @contract.accepts(str)
+    def attack_getter(name):
+        return Attack(*args[name])
+    return attack_getter, names
