3b0c68f334de81b922c81cb093e297d2078ec62e
Quinten <quintenpalmer@gmail.com>
2013-11-16 09:08:47 -0500

    Include the loaded json->classes in 'types'

--- engine/types.py
+++ engine/types.py
@@ -1,5 +1,13 @@
-from . import attack
+from . import attack, armor, tile, movement, unit
 
 attack_types = attack.load_attack_types()
 
 new_attack, attacks = attack.load_attacks(attack_types)
+
+new_armor, armors = armor.load_armors(attack_types)
+
+tiles = tile.load_tiles()
+
+new_movement, movements = movement.load_movements(tiles)
+
+new_unit, units = unit.load_units(new_attack, new_armor,  new_movement)
