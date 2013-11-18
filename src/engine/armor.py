16279b16f988ce5f34c15d0c4708d3a21a7bfb5b
Quinten <quintenpalmer@gmail.com>
2013-11-18 18:02:15 -0500

    Simply return of armor names

--- src/engine/armor.py
+++ src/engine/armor.py
@@ -10,16 +10,14 @@ class Armor(base.BaseDictionary):
 @contract.accepts(dict)
 def load_armors(attack_types):
     args = {}
-    names = []
     for armor_ in file_loader.read_and_parse_json('armors'):
         name = str(armor_['name'])
         resists = {}
         for attack_type, multiplier in armor_['resists'].items():
             resists[attack_types[str(attack_type)]] = multiplier
-        names.append(name)
         args[name] = resists
 
     @contract.accepts(str)
     def armor_getter(name):
         return Armor(dict(args[name]))
-    return armor_getter, names
+    return armor_getter, args.keys()
