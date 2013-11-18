1c882740e6ced8c66f3969b67ced48be650ad93f
Quinten <quintenpalmer@gmail.com>
2013-11-18 18:03:34 -0500

    More functional json loading

--- src/engine/file_loader.py
+++ src/engine/file_loader.py
@@ -6,7 +6,6 @@ data_dir = os.path.join(os.environ['PORTER'], 'data')
 
 def read_and_parse_json(data_type):
     sub_dir = os.path.join(data_dir, data_type)
-    elements = []
 
     def full_path(file_name):
         return os.path.join(sub_dir, file_name)
@@ -14,7 +13,8 @@ def read_and_parse_json(data_type):
     def only_json(file_name):
         return file_name.endswith('.json')
 
-    for json_file_name in filter(only_json, map(full_path, os.listdir(sub_dir))):
+    def load_json(json_file_name):
         with open(json_file_name) as json_file:
-            elements.append(json.load(json_file))
-    return elements
+            return json.load(json_file)
+
+    return map(load_json, filter(only_json, map(full_path, os.listdir(sub_dir))))
