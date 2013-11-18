fcb5911fe841e1b12b17e5884de69e3a51e81953
Quinten <quintenpalmer@gmail.com>
2013-11-17 22:49:22 -0500

    Include setup.py script

---
+++ bin/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+
+def main():
+    project_path = os.path.split(
+        os.path.dirname(
+            os.path.realpath(__file__)))[0]
+    export_project_path = 'export PORTER=%s' % (
+        project_path,)
+    export_python_path = 'export PYTHONPATH=$PYTHONPATH:%s' % (
+        os.path.join(
+            project_path, 'src'),)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-e', '--export-only',
+        action='store_true',
+        help='Only print the export lines')
+
+    args = parser.parse_args()
+    if args.export_only:
+        print export_project_path
+        print export_python_path
+    else:
+        print 'Setting up...'
+        print 'Either run the following or add it to your .bashrc or equivalent:'
+        print export_project_path
+        print export_python_path
+
+
+if __name__ == '__main__':
+    main()
