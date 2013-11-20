6baafa9226c9e02a1072981cfb5d44469ee766ef
Quinten <quintenpalmer@gmail.com>
2013-11-19 21:45:52 -0500

    Rename test_load_units to TestLoadUnits

--- tests/engine/file_loader_test.py
+++ tests/engine/file_loader_test.py
@@ -4,7 +4,7 @@ from engine import file_loader
 
 
 class FileLoaderTest(unittest.TestCase):
-    def test_load_units(self):
+    def testLoadUnits(self):
         dicts = file_loader.read_and_parse_json('units')
         self.assertIsInstance(dicts, list)
         self.assertGreater(len(dicts), 0)
