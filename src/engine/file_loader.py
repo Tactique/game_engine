1107fc26cf9baa235d62813ea0006687d4710280
Quinten <quintenpalmer@gmail.com>
2013-11-19 21:33:30 -0500

    Add load_enum and load_struct functions
    Load enumeration list json
    Load full struct into dictionary

--- src/engine/file_loader.py
+++ src/engine/file_loader.py
@@ -22,3 +22,23 @@ def read_and_parse_json(data_type):
             return json.load(json_file)
 
     return map(load_json, filter(only_json, map(full_path, os.listdir(sub_dir))))
+
+
+@contract.accepts(str)
+@contract.returns(dict)
+def load_enum(struct_name):
+    enum_ = {}
+    list_ = read_and_parse_json(struct_name)[0]
+    for enumeration, enum_type in enumerate(list_):
+        enum_[str(enum_type)] = enumeration
+    return enum_
+
+
+@contract.accepts(str)
+@contract.returns(dict)
+def load_struct(struct_name):
+    def create_struct_map(struct_map, struct_):
+        struct_map[str(struct_['name'])] = struct_
+        return struct_map
+
+    return reduce(create_struct_map, read_and_parse_json(struct_name), {})
