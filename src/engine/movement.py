b674d82d6d27228e6d469671a23e25b1a1459bc1
Quinten <quintenpalmer@gmail.com>
2013-11-19 21:38:49 -0500

    Use load_struct for movements

--- src/engine/movement.py
+++ src/engine/movement.py
@@ -9,18 +9,14 @@ class Movement(base.BaseDictionary):
 
 @contract.accepts(tile.Tiles)
 def load_movements(tiles):
-    args = {}
-    names = []
-    for movement_ in file_loader.read_and_parse_json('movements'):
-        name = str(movement_['name'])
-        speeds = {}
-        for tile_, multiplier in movement_['speeds'].items():
-            speeds[tiles[str(tile_)]] = multiplier
-        names.append(name)
-        args[name] = speeds
-
     @contract.accepts(str)
     @contract.returns(Movement)
     def movement_getter(name):
-        return Movement(dict(args[name]))
-    return movement_getter, names
+        movement_ = args[name]
+        speeds = {}
+        for tile_, multiplier in movement_['speeds'].items():
+            speeds[tiles[str(tile_)]] = multiplier
+        return Movement(speeds)
+
+    args = file_loader.load_struct('movements')
+    return movement_getter, args.keys()
