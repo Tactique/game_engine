5d9fb4e22f4fb6662110bf4b82c42a289dff318d
Quinten <quintenpalmer@gmail.com>
2013-11-20 18:33:13 -0500

    Use map for pep8 checking

--- bin/check.py
+++ bin/check.py
@@ -25,12 +25,14 @@ def find_all(directory, pattern):
 
 
 def pep8_all():
-    for file_ in find_all('.', '.py'):
+    def pep8_file(file_):
         checker = pep8.Checker(filename=file_, max_line_length=99)
         incorrect = checker.check_all()
         if incorrect != 0:
             raise Exception("pep8 on file %s failed" % (file_,))
         print '%s pep8 compliant' % (file_,)
+
+    map(pep8_file, find_all(os.environ['PORTER'], '.py'))
 
 
 def coverage_module(package, module):
