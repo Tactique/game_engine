0c3834fbd9327af23f244db133c1e279151bd952
Quinten <quintenpalmer@gmail.com>
2013-12-07 11:58:56 -0500

    Revamp data integrity check and use notify

--- bin/data_integrity_check.py
+++ bin/data_integrity_check.py
@@ -1,102 +1,90 @@
 #!/usr/bin/env python
 
 from engine import file_loader
+import notify
+
+notifier = notify.Notifier('Data Integrity')
 
 
-def verbose_print(line):
-    print 'OK: %s' % (line,)
-
-
-def verify_enum(dir_name, name):
-    attack_types = file_loader.read_and_parse_json(dir_name)[0]
-    if len(set(attack_types)) != len(attack_types):
-        raise Exception('%s | Duplicate(s) found : %s' % (name, ', '.join(attack_types),))
-    else:
-        verbose_print('%s | No duplicates found in : %s' % (name, ', '.join(attack_types),))
-    return attack_types
-
-
-def verify_contains(dir_name, module_name, sub_name, sub_set):
-    armors = file_loader.read_and_parse_json(dir_name)
-    for armor in armors:
-        name = armor['name']
-        used_resists = set()
-        for resist in armor[sub_name]:
-            used_resists.add(resist)
-        unused_resists = set(sub_set) - used_resists
-        unimplemented_resists = used_resists - set(sub_set)
-        if len(unused_resists) != 0:
-            raise Exception('%s | %s : Unused %s : %s' % (
-                module_name, name, sub_name, ', '.join(unused_resists),))
-        if len(unimplemented_resists) != 0:
-            raise Exception('%s | %s : Unimplemented %s : %s' % (
-                module_name, name, sub_name, ', '.join(unimplemented_resists),))
-        else:
-            verbose_print('%s | %s : implements all of %s' % (
-                module_name, name, ', '.join(used_resists)))
-    return armors
-
-
-def verify_used(dir_name, module_name, sub_name, sub_set):
-    used_attack_types = set()
-    attacks = file_loader.read_and_parse_json(dir_name)
-    for sub_attacks in attacks:
-        sub_ = sub_attacks[sub_name]
-        if not hasattr(sub_, '__iter__'):
-            sub_ = (sub_,)
-        for attack in sub_:
-            used_attack_types.add(attack)
-
-    unused_attack_types = set(sub_set) - used_attack_types
-    unimplemented_attack_types = used_attack_types - set(sub_set)
-    if len(unused_attack_types) != 0:
-        raise Exception('%s | Unused %s: %s' % (
-            module_name, sub_name, ', '.join(unused_attack_types)))
-    elif len(unimplemented_attack_types) != 0:
-        raise Exception('%s | Unimplemented %s: %s' % (
-            module_name, sub_name, ', '.join(unimplemented_attack_types)))
-    else:
-        verbose_print('%s | All %s used : %s' % (
-            module_name, sub_name, ', '.join(sub_set),))
-    return attacks
-
-
-def verify_attack_types():
-    return verify_enum('attack_types', 'Attack Type')
-
-
-def verify_attacks(attack_types):
-    return verify_used('attacks', 'Attack', 'attack_type', attack_types)
-
-
-def verify_armors(attack_types):
-    return verify_contains('armors', 'Armor', 'resists', attack_types)
-
-
-def verify_tiles():
-    return verify_enum('tiles', 'Tiles')
-
-
-def verify_movements(tiles):
-    return verify_contains('movements', 'Movement', 'speeds', tiles)
+def verify_all():
+    attack_types = verify_enum('attack_types')
+    attacks = verify_used('attacks', 'attack_type', attack_types)
+    armors = verify_contains('armors', 'resists', attack_types)
+    tiles = verify_enum('tiles')
+    movements = verify_contains('movements', 'speeds', tiles)
+    verify_units(attacks, armors, movements)
 
 
 def verify_units(attacks, armors, movements):
     def get_name(dict_):
         return dict_['name']
 
-    verify_used('units', 'Unit', 'attacks', map(get_name, attacks))
-    verify_used('units', 'Unit', 'armor', map(get_name, armors))
-    verify_used('units', 'Unit', 'movement', map(get_name, movements))
+    verify_used('units', 'attacks', map(get_name, attacks))
+    verify_used('units', 'armor', map(get_name, armors))
+    verify_used('units', 'movement', map(get_name, movements))
 
 
-def verify_all():
-    attack_types = verify_attack_types()
-    attacks = verify_attacks(attack_types)
-    armors = verify_armors(attack_types)
-    tiles = verify_tiles()
-    movements = verify_movements(tiles)
-    units = verify_units(attacks, armors, movements)
+def verify_enum(module):
+    attack_types = file_loader.read_and_parse_json(module)[0]
+    if len(set(attack_types)) != len(attack_types):
+        print_failure(module, '', 'Duplicates', attack_types)
+    else:
+        print_success(module, '', 'No duplicates', attack_types)
+    return attack_types
+
+
+def verify_contains(module, sub_name, sub_set):
+    armors = file_loader.read_and_parse_json(module)
+    for armor in armors:
+        instance = armor['name']
+        used = set()
+        for resist in armor[sub_name]:
+            used.add(resist)
+
+        _verify_exact_subset(module, sub_name, used, sub_set)
+    return armors
+
+
+def verify_used(module, sub_name, sub_set):
+    used = set()
+    attacks = file_loader.read_and_parse_json(module)
+    for sub_attacks in attacks:
+        sub_ = sub_attacks[sub_name]
+        if not hasattr(sub_, '__iter__'):
+            sub_ = (sub_,)
+        for attack in sub_:
+            used.add(attack)
+
+    _verify_exact_subset(module, sub_name, used, sub_set)
+
+    return attacks
+
+
+def _verify_exact_subset(module, sub_name, used, sub_set):
+    unused = set(sub_set) - used
+    unimplemented = used - set(sub_set)
+    if len(unused) != 0:
+        print_failure(module, sub_name.title(), 'Unused', unused)
+    elif len(unimplemented) != 0:
+        print_failure(module, sub_name.title(), 'Unimplemented', unimplemented)
+    else:
+        print_success(module, sub_name.title(), 'All used', used)
+
+
+def print_failure(module, instance, message, components):
+    notifier.failure(_print_message(module, instance, message, components))
+
+
+def print_success(module, instance, message, components):
+    notifier.success(_print_message(module, instance, message, components))
+
+
+def _print_message(module, instance, message, components, pretty=True):
+    return '%s -> %s %s : %s' % (
+        notify.trail_spaces(module.title(), 12),
+        notify.trail_spaces(instance.title(), 12),
+        notify.trail_spaces(message, 14),
+        ', '.join(components))
 
 
 if __name__ == '__main__':
