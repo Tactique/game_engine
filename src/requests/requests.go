8387ae148bc174c5cb3acba0dc4262f91b34778a
Quinten Palmer <quintenpalmer@gmail.com>
2014-04-24 17:52:16 -0400

    All request commands and data structures

---
+++ src/requests/requests.go
@@ -0,0 +1,52 @@
+package requests
+
+type NewCommandRequest struct {
+    Uids []int `json:"uids"`
+}
+
+type ViewCommandRequest struct {
+}
+
+type MoveCommandRequest struct {
+    Move []LocationStruct `json:"move"`
+}
+
+type EndTurnCommandRequest struct {
+    PlayerId int `json:"playerId"`
+}
+
+type ExitCommandRequest struct {
+    Reason string `json:"reason"`
+}
+
+type WorldStruct struct {
+    Terrain [][]int `json:"terrain"`
+    Units []*UnitStruct `json:"units"`
+    Players []*PlayerStruct `json:"players"`
+    TurnOwner int `json:"turnOwner"`
+}
+
+type PlayerStruct struct {
+    PlayerId int `json:"playerId"`
+    Nation int `json:"nation"`
+    Team int `json:"team"`
+}
+
+type UnitStruct struct {
+    Name string `json:"name"`
+    Health int `json:"health"`
+    Nation int `json:"nation"`
+    Movement *MovementStruct `json:"movement"`
+    Position *LocationStruct `json:"position"`
+    Distance int `json:"distance"`
+}
+
+type MovementStruct struct {
+    Name string `json:"name"`
+    Speeds map[string]float64 `json:"speeds"`
+}
+
+type LocationStruct struct {
+    X int `json:"x"`
+    Y int `json:"y"`
+}
