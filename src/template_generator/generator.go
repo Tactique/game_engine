ed291383f051b3f77f7a51afa88d097e96ef5127
Quinten Palmer <quintenpalmer@gmail.com>
2014-04-25 14:39:13 -0400

    Template generator

---
+++ src/template_generator/generator.go
@@ -0,0 +1,34 @@
+package template_generator
+
+import (
+    "fmt"
+    "encoding/json"
+    "requests"
+)
+
+func GenerateAllTemplates() {
+    ret_game := &requests.WorldStruct{
+        Terrain: [][]int{
+            []int{0, 0, 0},
+            []int{0, 0, 0},
+            []int{0, 0, 0}},
+        Units: []*requests.UnitStruct{
+            &requests.UnitStruct{
+                Name: "Tank", Health: 10, Nation: 0,
+                Movement: &requests.MovementStruct{
+                    Type: "Treads", Speeds: map[string]float64{
+                        "0": 1}},
+                Position: &requests.LocationStruct{X: 0, Y: 0},
+                Distance: 10}},
+        Players: []*requests.PlayerStruct{
+            &requests.PlayerStruct{
+                PlayerId: 26, Nation: 0, Team: 0},
+            &requests.PlayerStruct{
+                PlayerId: 13, Nation: 1, Team: 1}},
+        TurnOwner: 0}
+    jsonString, err := json.MarshalIndent(ret_game, "", "    ")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(string(jsonString))
+}
---
+++ templater.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "template_generator"
+)
+
+func main() {
+    template_generator.GenerateAllTemplates()
+}
+
