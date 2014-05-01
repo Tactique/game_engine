3c969cd1c3f37db6dcf5a40d2f21d7e5dea416ed
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-01 18:17:22 -0400

    Move templater into standalone and out of dummy_client

--- templater.go
+++ templater.go
@@ -1,9 +1,88 @@
 package main
 
 import (
-    "dummy_client"
+	"dummy_client"
+    "encoding/json"
+    "net"
+    "io/ioutil"
+    "bytes"
 )
 
 func main() {
-    dummy_client.Template()
+	conn, err := net.Dial("tcp", "localhost:5269")
+	if err != nil {
+		panic(err)
+	}
+    client := dummy_client.NewClient(conn)
+	message, err := client.NewGame()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.ViewWorld()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.ViewPlayers()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.ViewUnits()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.ViewTerrain()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.Move()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.Attack()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+	message, err = client.Turn()
+	if err != nil {
+		panic(err)
+	}
+    writeJson(message)
+}
+
+func writeJson(message []byte) {
+    name, contents := splitOnce(message)
+    writableContents := transformResponse(name, contents)
+    ioutil.WriteFile("templates/" + string(name) + ".json", writableContents, 0644)
+}
+
+func transformResponse(name []byte, message []byte) []byte {
+    response := make(map[string]interface{}, 0)
+	err := json.Unmarshal(message, &response)
+	if err != nil {
+        panic(err)
+	}
+    writableResponse, err := json.MarshalIndent(map[string]interface{}{
+        string(name): response["payload"]}, "", "    ")
+	if err != nil {
+        panic(err)
+	}
+    return writableResponse
+}
+
+func splitOnce(input []byte) ([]byte, []byte) {
+	pieces := bytes.SplitN(input, []byte(":"), 2)
+	if len(pieces) == 1 {
+		return pieces[0], []byte{}
+	} else if len(pieces) == 2 {
+		return pieces[0], pieces[1]
+	} else {
+		return []byte{}, []byte{}
+	}
 }
