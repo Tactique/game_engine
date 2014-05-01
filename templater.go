57344ecc8777ed4f9dd6598ef497c873d5cbd07a
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-01 18:21:24 -0400

    Fix up templater error cases (mkdir and write error)

--- templater.go
+++ templater.go
@@ -1,79 +1,88 @@
 package main
 
 import (
+	"bytes"
 	"dummy_client"
-    "encoding/json"
-    "net"
-    "io/ioutil"
-    "bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
 )
 
 func main() {
+	err := os.Mkdir("templates/", 0777)
+	if err != nil {
+		fmt.Println("Couldn't make templates/ :", err)
+	}
 	conn, err := net.Dial("tcp", "localhost:5269")
 	if err != nil {
 		panic(err)
 	}
-    client := dummy_client.NewClient(conn)
+	client := dummy_client.NewClient(conn)
 	message, err := client.NewGame()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.ViewWorld()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.ViewPlayers()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.ViewUnits()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.ViewTerrain()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.Move()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.Attack()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 	message, err = client.Turn()
 	if err != nil {
 		panic(err)
 	}
-    writeJson(message)
+	writeJson(message)
 }
 
 func writeJson(message []byte) {
-    name, contents := splitOnce(message)
-    writableContents := transformResponse(name, contents)
-    ioutil.WriteFile("templates/" + string(name) + ".json", writableContents, 0644)
+	name, contents := splitOnce(message)
+	writableContents := transformResponse(name, contents)
+	err := ioutil.WriteFile("templates/"+string(name)+".json", writableContents, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func transformResponse(name []byte, message []byte) []byte {
-    response := make(map[string]interface{}, 0)
+	response := make(map[string]interface{}, 0)
 	err := json.Unmarshal(message, &response)
 	if err != nil {
-        panic(err)
+		panic(err)
 	}
-    writableResponse, err := json.MarshalIndent(map[string]interface{}{
-        string(name): response["payload"]}, "", "    ")
+	writableResponse, err := json.MarshalIndent(map[string]interface{}{
+		string(name): response["payload"]}, "", "    ")
 	if err != nil {
-        panic(err)
+		panic(err)
 	}
-    return writableResponse
+	return writableResponse
 }
 
 func splitOnce(input []byte) ([]byte, []byte) {
