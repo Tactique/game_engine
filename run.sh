5fb066ebc24d279cf72d9488ca61820c4eab1f8a
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-05 19:17:17 -0400

    Forward run.sh to log until using logger

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ pushd $(dirname $0) > /dev/null
 
 pkill tactique_engine
 GOPATH=$(pwd) go build -o tactique_engine engine.go
-./tactique_engine &
+./tactique_engine > engine.log &
 
 popd > /dev/null
