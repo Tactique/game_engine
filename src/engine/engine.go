1dacdcf019b6a5c7eaed8f6298c8dfc5877e527b
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-07 13:01:18 -0400

    Pull templater into engine module

--- src/engine/engine.go
+++ src/engine/engine.go
@@ -2,15 +2,23 @@ package engine
 
 import (
 	"flag"
+	"os"
 	"github.com/Tactique/golib/logger"
 )
 
 func Main() {
 	var port = flag.Int("port", 5269, "path to write the generated json")
+	var write_template = flag.Bool("write_templates", false, "whether to write the template response json")
+	var template_out_dir = flag.String("template_out_dir", ".", "path to write the generated template json")
 	flag.Parse()
 
 	logger.SetupLoggerHelper("engine.log")
 
-	logger.Infof("Starting Tactique on port %d!", *port)
-	ListenForever(*port)
+	if *write_template {
+		generateTemplates(*template_out_dir)
+		os.Exit(0)
+	} else {
+		logger.Infof("Starting Tactique on port %d!", *port)
+		ListenForever(*port)
+	}
 }
--- src/engine/templater.go
+++ src/engine/templater.go
@@ -1,16 +1,14 @@
-package templater
+package engine
 
 import (
 	"dummy_client"
-	"engine"
 	"io/ioutil"
-	"bytes"
 	"encoding/json"
 	"path"
 )
 
-func GenerateTemplates(filepath string) {
-	handler := engine.NewRequestHandler()
+func generateTemplates(filepath string) {
+	handler := NewRequestHandler()
 
 	message := handler.HandleRequest(dummy_client.BuiltNewRequest)
 	writeJson(filepath, message)
@@ -51,15 +49,4 @@ func transformResponse(name []byte, message []byte) []byte {
 		panic(err)
 	}
 	return writableResponse
-}
-
-func splitOnce(input []byte) ([]byte, []byte) {
-	pieces := bytes.SplitN(input, []byte(":"), 2)
-	if len(pieces) == 1 {
-		return pieces[0], []byte{}
-	} else if len(pieces) == 2 {
-		return pieces[0], pieces[1]
-	} else {
-		return []byte{}, []byte{}
-	}
 }
