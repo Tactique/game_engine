3ef0df55e1f69c9c41722ac068b7941e419ba569
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-09 07:55:05 -0400

    Forward stdout to /dev/null to actually detach to run in the background

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ pushd $(dirname $0) > /dev/null
 
 pkill tactique_engine
 GOPATH=$(pwd) go build -o tactique_engine engine.go
-./tactique_engine -logpath=engine.log &
+./tactique_engine -logpath=engine.log > /dev/null &
 
 popd > /dev/null
